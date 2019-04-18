112dbef61d5c24f1a01881f4ea672ab977245492
ctghadiri <ctghadiri@gmail.com>
2019-04-18 16:27:34 -0700

    add require for models and some pseudocode

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ var express = require("express");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// require models + port
+var db = require("./models")
+// Initialize Express
+
+// Configure middleware
+
+// Connect to mongo
+
+// Get routes
+    // Scraping
+    // Getting Info from DB
+
+// Start Server
