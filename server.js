1168753db43e03c795a05dc1d7a2a8d2f420a765
ctghadiri <ctghadiri@gmail.com>
2019-04-18 15:31:52 -0700

    create .gitignore adding dsstore and node mudules and create initial files and folders. server has requirements as well for packages

---
+++ server.js
@@ -0,0 +1,5 @@
+// require express, mongoose, cheerio, axios
+var express = require("express");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
