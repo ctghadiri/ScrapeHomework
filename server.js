081b76f973a28c9fa89090245f9c61a8a9b5aa3c
ctghadiri <ctghadiri@gmail.com>
2019-04-18 16:31:52 -0700

    add port and initialize express

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // require models + port
-var db = require("./models")
+var db = require("./models");
+var PORT = 3000;
 // Initialize Express
-
+var app = express()
 // Configure middleware
 
 // Connect to mongo
