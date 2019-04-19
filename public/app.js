1dc1e226ced4698cc706266b362e86febbc0f443
ctghadiri <ctghadiri@gmail.com>
2019-04-19 15:56:21 -0700

    add method to append json data to page

---
+++ public/app.js
@@ -0,0 +1,7 @@
+// Grab movies data and display on page
+
+$.getJSON("/Movies", function (data){
+    for (var i = 0; i < data.length; i++){
+        $("movies").append("<p data-id='" + data[i]._id + "'>" + data[i].movie + "</p>")
+    }
+})
