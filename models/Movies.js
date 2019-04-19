823e5ef00af85d2b278af18eba6afaa9deb376ec
ctghadiri <ctghadiri@gmail.com>
2019-04-19 09:35:12 -0700

    create constructor

--- models/Movies.js
+++ models/Movies.js
@@ -2,4 +2,17 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
+var MovieSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    Note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+var Movies = mongoose.model("Movie", MovieSchema)
+
 module.exports = Movies
