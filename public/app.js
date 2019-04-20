b8884c4316c39a258c516e6c0df986790fa32895
ctghadiri <ctghadiri@gmail.com>
2019-04-19 17:49:58 -0700

    add octothorpe for id

--- public/app.js
+++ public/app.js
@@ -2,6 +2,6 @@
 
 $.getJSON("/Movies", function (data){
     for (var i = 0; i < data.length; i++){
-        $("movies").append("<p data-id='" + data[i]._id + "'>" + data[i].movie + "</p>")
+        $("#movies").append("<p data-id='" + data[i]._id + "'>" + data[i].movie + "</p>")
     }
 })
