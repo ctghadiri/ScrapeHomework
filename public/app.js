869af3269c294e1734b75e6f124cfa390a562380
ctghadiri <ctghadiri@gmail.com>
2019-04-19 18:01:56 -0700

    add click listener and ajax call for ptags

--- public/app.js
+++ public/app.js
@@ -1,7 +1,26 @@
+import { get } from "http";
+
 // Grab movies data and display on page
 
 $.getJSON("/Movies", function (data){
     for (var i = 0; i < data.length; i++){
         $("#movies").append("<p data-id='" + data[i]._id + "'>" + data[i].movie + "</p>")
     }
+})
+
+
+$(document).on("click", "p", function (){
+
+    $("#note").empty()
+
+    var id = $(this).attr("data-id");
+
+    $.ajax({
+        method: get,
+        url: "/movies/" + id
+    })
+    .then(function(data){
+        
+    })
+
 })
