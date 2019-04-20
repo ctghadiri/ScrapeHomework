268c2b31e34dd0c5131eaeabcec292a6a4c69c35
ctghadiri <ctghadiri@gmail.com>
2019-04-19 20:27:50 -0700

    add styling of css for div with movies and notes

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 
 $.getJSON("/Movies", function (data){
     for (var i = 0; i < data.length; i++){
-        $("#movies").append("<p data-id='" + data[i]._id + "'>" + data[i].movie + "<br />" + data[i].link + "</p>")
+        $("#movies").append("<p data-id='" + data[i]._id + "'>" + data[i].movie + "<br />" + "<button onclick='location.href=" +data[i].link + " type= 'button'> Link </button>")
     }
 })
 
